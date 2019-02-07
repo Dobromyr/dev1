416a0b1ae1269873f1e5c8b306ba56641123e127
Dobromyr <vartsabiuk@gmail.com>
2019-02-07 22:51:46 +0200

    Ordering

---
+++ script/ls_tar.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+DATE=$(date +%H_%M_%S)
+DIR="/home/dobromyr/SH/1"
+for i in *
+do
+  echo $i
+done
+#echo $DATE
+tar -czvf $DIR/test_$DATE.tar.gz $DIR/*.txt
+find  -name "*.tar.gz" -type f -mmin +3 -delete
